140e16fd6d5328507c2c65387dbceb72bd2f3e6f
EverthonH <Everthon.henrique.75@gmail.com>
2021-09-16 00:34:11 -0300

    Ajustando seeder da tabela de atendimentos

--- database/seeders/AttendanceSeeder.php
+++ database/seeders/AttendanceSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\Attendance;
+use \App\Models\User;
+use \App\Models\Patient;
 
 class AttendanceSeeder extends Seeder
 {
@@ -14,6 +16,13 @@ class AttendanceSeeder extends Seeder
      */
     public function run()
     {
-        Attendance::factory(9)->create();
+        for($x = 0; $x < 3; $x++){
+            for($y = 1; $y <= 3; $y++) {
+                 Attendance::factory(1)->create([
+                      'user_id' => User::find($x + 1),
+                      'patient_id' => Patient::find($y + ($x * 3)),
+                 ]);
+            }
+       }
     }
 }
