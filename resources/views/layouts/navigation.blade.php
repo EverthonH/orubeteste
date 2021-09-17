a89a1278bb6e9fd72a1b258f6eee309a9e6367c3
EverthonH <Everthon.henrique.75@gmail.com>
2021-09-17 01:54:23 -0300

    Alterando logo da aplicação

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -6,14 +6,14 @@
                 <!-- Logo -->
                 <div class="flex-shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-10 w-auto fill-current text-gray-600" />
+                        <img src="{{asset('img/fav.png')}}" alt="" class="h-10 w-10">
                     </a>
                 </div>
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                        {{ __('Orube Saúde') }}
                     </x-nav-link>
                 </div>
             </div>
