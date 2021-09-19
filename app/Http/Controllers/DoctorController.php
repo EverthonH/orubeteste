6b6360060440baee6245a2a066aff23ca5aa7990
EverthonH <Everthon.henrique.75@gmail.com>
2021-09-19 05:24:19 -0300

    Salvando novos médicos e pacientes

--- app/Http/Controllers/DoctorController.php
+++ app/Http/Controllers/DoctorController.php
@@ -12,5 +12,30 @@ class DoctorController extends Controller
         $doctors_list = Doctor::all();
         return view('dashboard', ['doctors_list' => $doctors_list]);
     }
+    public function create()
+    {
+        return view('dashboard', ['create_doctor' => true]);
+    }
+    public function store(Request $request)
+    {
+        $request->validate([
+            
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:doctors'],
+            'cpf' => ['required', 'string', 'min:11', 'max:14', 'unique:doctors'],
+            'crm' => ['required','max:14','unique:doctors'],
+            'cell_phone' => ['required', 'min:11','max:18'],
+        ]);
+        Doctor::create([
+            'name'=> $request->name,
+            'email'=> $request->email,
+            'cpf'=> $request->cpf,
+            'crm'=> $request->crm,
+            'birth'=> $request->birth,
+            'cell_phone'=> $request->cell_phone,
+        ]);
+
+        return redirect('dashboard');
+    }
 
 }
--- app/Http/Controllers/PatientController.php
+++ app/Http/Controllers/PatientController.php
@@ -12,4 +12,39 @@ class PatientController extends Controller
         $patients_list = Patient::all();
         return view('dashboard', ['patients_list' => $patients_list]);
     }
+    public function create()
+    {
+        return view('dashboard', ['create_patient' => true]);
+    }
+    public function store(request $request){
+
+        $request->validate([
+            
+            'cpf' => ['required','string', 'min:11', 'max:14','unique:patients'],
+            'name' => ['required', 'string', 'max:255'],
+            'birth' => ['required'],
+            'sex' => ['required'],
+            'height' => ['required'],
+            'health_insurance' => ['required'],
+            'weight' => ['required'],
+            'blood_type' => ['required','min:2', 'max:3'],
+            'cell_phone' => ['required', 'min:11','max:18'],
+            'comment' => ['max:1024'],
+        ]);
+        
+        Patient::create([
+            'cpf'=> $request->cpf,
+            'name'=> $request->name,
+            'birth'=> $request->birth,
+            'sex'=> $request->sex,
+            'height'=> $request->height,
+            'health_insurance'=> $request->health_insurance,
+            'weight'=> $request->weight,
+            'blood_type'=> $request->blood_type,
+            'cell_phone'=> $request->cell_phone,
+            'comment'=> $request->comment,
+        ]);
+
+        return redirect('dashboard');
+    }
 }
