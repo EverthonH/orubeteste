0d0452dbba195780145dd04ff24ce140813badf9
EverthonH <Everthon.henrique.75@gmail.com>
2021-09-19 08:12:19 -0300

    Adicionando modal para visualisar todas as informações de médico, peciente e atendimento

--- resources/views/components/list-table.blade.php
+++ resources/views/components/list-table.blade.php
@@ -48,16 +48,124 @@ if($entity == 'attendances'){
                     <div>{{$element->birth}}</div>
                 @endif
                 {{-- Ações --}}
-                <div class="grid grid-cols-3 m-auto gap-3 ">
+                <div class="grid grid-cols-3 m-auto gap-3" x-date="{click: false}">
                     {{-- Visualizar --}}
-                    <a href="">
-                        <div> 
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                            <path d="M10 12a2 2 0 100-4 2 2 0 000 4z" />
-                            <path fill-rule="evenodd" d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z" clip-rule="evenodd" />
-                            </svg>
-                        </div> 
-                    </a>
+                        <div>
+                            <div x-data="{ add_modal: false }">
+                                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                                    <div class="">
+                                        <div class="">
+                                            <div @click="add_modal = true">
+                                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                                                    <path d="M10 12a2 2 0 100-4 2 2 0 000 4z" />
+                                                    <path fill-rule="evenodd" d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z" clip-rule="evenodd" />
+                                                </svg>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="fixed z-10 inset-0 overflow-y-auto" style="display: none;" x-show="add_modal">
+                                    <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+                                        <div class="fixed inset-0 transition-opacity" aria-hidden="true" @click="add_modal = false">
+                                            <div class="absolute inset-0 bg-gray-500 opacity-75"></div>
+                                        </div>
+                                        <span class="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
+                                        <div class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full" role="dialog" aria-modal="true" aria-labelledby="modal-headline">
+                                            <div class="p-3">
+                                                @if($entity == 'attendances')
+                                                    <h1>Detalhes do atendimento</h1>
+                                                    <div>
+                                                        Nome do médico: {{$element->doctor->name}}
+                                                    </div>
+                                                    <div>
+                                                        CPF do médico: {{$element->doctor->cpf}}
+                                                    </div>
+                                                    <div>
+                                                        Nome do paciente: {{$element->patient->name}}
+                                                    </div>
+                                                    <div>
+                                                        CPF do paciente: {{$element->patient->cpf}}
+                                                    </div>
+                                                    <div>
+                                                        @php
+                                                        $element_hour = $element->hour;
+                                                        $date_hour = explode(' ', $element_hour);
+                                                        $date = $date_hour[0];
+                                                        $hour = $date_hour[1];
+                                                        @endphp
+                                                         Data: {{$date}}
+                                                    </div>
+                                                    <div>
+                                                        Horário: {{$hour}}
+                                                    </div>
+                                                    <div>
+                                                        Sala: {{$element->romm}}
+                                                    </div>
+                                                    <div>
+                                                        Observações: {{$element->complaint}}
+                                                    </div>
+                                                    <div>
+                                                        Procedimentos: {{$element->procedure}}
+                                                    </div>
+                                                @elseif($entity == 'doctors')
+                                                    <h1>Detalhes do atendimento</h1>
+                                                    <div>
+                                                        Nome: {{$element->name}}
+                                                    </div>
+                                                    <div>
+                                                        CPF{{$element->cpf}}
+                                                    </div>
+                                                    <div>
+                                                        CRM: {{$element->crm}}
+                                                    </div>
+                                                    <div>
+                                                        E-mail: {{$element->email}}
+                                                    </div>
+                                                     <div>
+                                                        Número do celular: {{$element->cell_phone}}
+                                                    </div>
+                                                    <div>
+                                                        Nascimento: {{$element->birth}}
+                                                    </div>
+
+                                                @else
+                                                    <h1>Detalhes do paciente</h1>
+                                                    <div>
+                                                        Nome: {{$element->name}}
+                                                    </div>
+                                                    <div>
+                                                        CPF: {{$element->cpf}}
+                                                    </div>
+                                                    <div>
+                                                        Sexo: {{$element->sex}}
+                                                    </div>
+                                                    <div>
+                                                        Altura: {{$element->height}}
+                                                    </div>
+                                                    <div>
+                                                        Peso: {{$element->weight}}
+                                                    </div>
+                                                    <div>
+                                                        Tipo sanguíneio: {{$element->blood_type}}
+                                                    </div>
+                                                    <div>
+                                                        Plano de saúde: {{$element->health_insurance}}
+                                                    </div>
+                                                    <div>
+                                                        {{$element->comment}}
+                                                    </div>
+
+                                                @endif
+
+                                                <a class="underline text-sm text-red-600 hover:text-red-900" href="#"@click="add_modal=false">
+                                                    {{ __('Fechar') }}
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     {{-- Editar --}}
                     <a href="">
                     <div>
